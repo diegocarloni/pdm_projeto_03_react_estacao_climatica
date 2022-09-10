f95fd34861ab0e3d76c154e628523a6272a61c35
Diego Carloni <diego.carloni@hotmail.com>
2022-09-09 21:26:11 -0300

    apostila05: fim do bloco de codigo 2.6.1
    implementacao da funcao que calcula a estacao climatica do usuario

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-class App extends React.Component{
-
 /*Função que define um componente react
 export default function App (){
 
@@ -15,6 +13,38 @@ export default function App (){
             console.log(position)
         }    
     )*/
+
+class App extends React.Component{
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            latitude: null,
+            langitude: null,
+            estacao: null, 
+            data: null,
+            icone: null
+        }
+    }
+
+    obterEstacao = (data, latitude) => {
+        const ano = data.getFullYear();
+        const d1 = new Date(ano, 5, 21)
+        const d2 = new Date(ano, 8, 24)
+        const d3 = new Date(ano, 11, 22)
+        const d4 = new Date(ano, 3, 21)
+        const sul = latitude < 0
+        if (data >= d1 && data < d2){
+            return sul ? 'Inverno' : 'Verão'
+        }
+        if (data >= d2 && data < d3){
+            return sul ? 'Primavera' : 'Outono'
+        }
+        if (data >= d3 && data < d4){
+            return sul ? 'Verão' : 'Inverno'
+        }
+        return sul ? 'Outono' : 'Primavera'
+    }
         render(){
         return(
             <div>
